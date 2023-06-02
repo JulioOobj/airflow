e494fc47d07d1d775c51d51b651ece454cc4f3d2
JulioOobj <julio.maruyama@oobj.com.br>
2023-06-02 19:46:15 -0300

    teeeste

--- dags/teste.py
+++ dags/teste.py
@@ -20,66 +20,71 @@ This is an example DAG which uses SparkKubernetesOperator and SparkKubernetesSen
 In this example, we create two tasks which execute sequentially.
 The first task is to submit sparkApplication on Kubernetes cluster(the example uses spark-pi application).
 and the second task is to check the final state of the sparkApplication that submitted in the first state.
+
 Spark-on-k8s operator is required to be already installed on Kubernetes
 https://github.com/GoogleCloudPlatform/spark-on-k8s-operator
 """
 
-from datetime import timedelta, datetime
+import os
+from datetime import datetime, timedelta
 
 # [START import_module]
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
+
 # Operators; we need this to operate!
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
-from airflow.utils.dates import days_ago
 
 # [END import_module]
 
-# [START default_args]
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': days_ago(1),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'max_active_runs': 1,
-    'retries': 3
-}
-# [END default_args]
 
 # [START instantiate_dag]
 
-dag = DAG(
-    dag_id="pyspark",
-    default_args=default_args,
+
+
+[docs]ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
+
+
+[docs]DAG_ID = "spark_pi"
+
+
+with DAG(
+    DAG_ID,
+    default_args={'max_active_runs': 1},
+    description='submit spark-pi as sparkApplication on kubernetes',
     schedule_interval=timedelta(days=1),
-    tags=['example']
-)
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+) as dag:
+    # [START SparkKubernetesOperator_DAG]
 
-# spark = open(
-#     "example_spark_kubernetes_operator_pi.yaml").read()
+[docs]    t1 = SparkKubernetesOperator(
+        task_id='spark_pi_submit',
+        namespace="default",
+        application_file="example_spark_kubernetes_spark_pi.yaml",
+        do_xcom_push=True,
+        dag=dag,
 
-submit = SparkKubernetesOperator(
-    task_id="spark_pi_submit",
-    namespace="sampletenant",
-    application_file="spark-base.yaml",
-    kubernetes_conn_id="kubernetes_in_cluster",
-    do_xcom_push=True,
-    dag=dag,
-    enable_impersonation_from_ldap_user=False
-)
+    )
 
-sensor = SparkKubernetesSensor(
-    task_id="spark_pi_monitor",
-    namespace="sampletenant",
-    application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
-    kubernetes_conn_id="kubernetes_in_cluster",
-    dag=dag,
-    attach_log=True
-)
+    t2 = SparkKubernetesSensor(
+        task_id='spark_pi_monitor',
+        namespace="default",
+        application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
+        dag=dag,
+    )
+    t1 >> t2
 
-submit >> sensor
+    # [END SparkKubernetesOperator_DAG]
+    from tests.system.utils.watcher import watcher
+
+    # This test needs watcher in order to properly mark success/failure
+    # when "tearDown" task with trigger rule is part of the DAG
+    list(dag.tasks) >> watcher()
+
+from tests.system.utils import get_test_run  # noqa: E402
+
+# Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
+
+[docs]test_run = get_test_run(dag)
