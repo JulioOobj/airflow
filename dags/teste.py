7938086455e53a124b6ed90be84c66ee88e6de08
JulioOobj <julio.maruyama@oobj.com.br>
2023-06-03 10:50:11 -0300

    aaaaa

--- dags/teste.py
+++ dags/teste.py
@@ -74,7 +74,6 @@ with DAG(
         kubernetes_conn_id="kubeConnTest",
         namespace="default",
         aplication_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
-        #aplication_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')}}",
         dag=dag,
     )
     t1 >> t2
