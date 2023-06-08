58fe716b1f6e7d16c16d0de07a49c33075e84638
JulioOobj <julio.maruyama@oobj.com.br>
2023-06-08 10:11:20 -0300

    xx

--- dags/teste.py
+++ dags/teste.py
@@ -73,7 +73,7 @@ t2 = SparkKubernetesSensor(
     task_id='spark_pi_monitor',
     kubernetes_conn_id="kubeConnTest",
     namespace="default",
-    application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit') }}",
+    application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
     dag=dag,
 )
 t1 >> t2
